2d147984f9a430c71eacfa00f78db20e81b7d572
Gyurka Dora <dora.gyurka25@gmail.com>
2026-08-12 15:07:43 +0200

    feature: Add partial login files

---
+++ backend/src/main/java/com/example/demo/dto/request/LogInRequest.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LogInRequest {
+
+    private String email;
+    private String password;
+}
