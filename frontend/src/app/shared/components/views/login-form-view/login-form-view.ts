6b7febfa571e0302482903302070f568710b5368
Sonia Damian <sonjadamian@gmail.com>
2026-08-13 13:46:21 +0300

    fix:reformat login page

--- frontend/src/app/shared/components/views/login-form-view/login-form-view.ts
+++ frontend/src/app/shared/components/views/login-form-view/login-form-view.ts
@@ -3,10 +3,19 @@ import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { TranslatePipe } from '@ngx-translate/core';
 import { GenericFormView } from '../generic-form/generic-form.view';
 import { RouterLink } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login-form-view',
-  imports: [ReactiveFormsModule, GenericFormView, TranslatePipe, RouterLink],
+  imports: [
+    ReactiveFormsModule,
+    GenericFormView,
+    TranslatePipe,
+    RouterLink,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './login-form-view.html',
   styleUrl: './login-form-view.css',
 })
--- frontend/src/app/shared/pages/login/login-page.ts
+++ frontend/src/app/shared/pages/login/login-page.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { LoginFormContainerComponent } from '../../components/containers/login-form-container';
 import { ToolbarView } from '../../components/views/toolbar/toolbar.view';
 import { LanguageSwitcherContainer } from '../../components/containers/language-switcher.container';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [LoginFormContainerComponent, ToolbarView, LanguageSwitcherContainer],
+  imports: [LoginFormContainerComponent, ToolbarView, LanguageSwitcherContainer, TranslatePipe],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
