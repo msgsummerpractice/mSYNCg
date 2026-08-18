f7c13b304a904590b2294c5937706e0088f87f8f
Chertes Victor <victor.chertes.vc@gmail.com>
2026-08-18 15:10:21 +0300

    fix: remove unused import

--- backend/src/test/java/com/example/demo/controller/EventControllerTests.java
+++ backend/src/test/java/com/example/demo/controller/EventControllerTests.java
@@ -29,7 +29,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
