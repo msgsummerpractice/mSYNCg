79ebd2950a9ea1ce624a1b8781c996c50557de1a
Raul Simon <raulsimonalexas@yahoo.com>
2026-08-19 12:13:28 +0300

    chore: remove logic use for testing

--- frontend/src/app/features/event/components/containers/event-card.container.ts
+++ frontend/src/app/features/event/components/containers/event-card.container.ts
@@ -6,7 +6,6 @@ import { finalize } from 'rxjs';
 import { EventCardView } from '../views/event-card/event-card.view';
 import { Event as AppEvent } from '../../../../core/models/event.model';
 import { EventService } from '../../../../core/services/event.service';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-event-card-container',
@@ -22,8 +21,6 @@ export class EventCardContainer {
   private readonly destroyRef = inject(DestroyRef);
   private readonly eventService = inject(EventService);
   private readonly platformId = inject(PLATFORM_ID);
-  //for testing
-  private readonly route = inject(ActivatedRoute);
 
   readonly event = signal<AppEvent | null>(null);
   readonly isLoading = signal(false);
@@ -38,20 +35,9 @@ export class EventCardContainer {
         this.loadEvent(id);
       }
     });
-
-    //for testing
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-
-    if (Number.isInteger(id) && id > 0) {
-      this.loadEvent(id);
-    }
   }
 
   private loadEvent(id: number): void {
-    if (!isPlatformBrowser(this.platformId)) {
-      return;
-    }
-
     this.isLoading.set(true);
 
     this.eventService
