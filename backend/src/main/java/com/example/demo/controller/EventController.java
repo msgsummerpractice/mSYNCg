44437a784404020c27c186cffe50497da119f35a
Gyurka Dora <dora.gyurka25@gmail.com>
2026-08-20 12:14:12 +0200

    fix: fixed authorization for create event

--- backend/src/main/java/com/example/demo/controller/EventController.java
+++ backend/src/main/java/com/example/demo/controller/EventController.java
@@ -37,7 +37,7 @@ public class EventController {
 
     private final EventService eventService;
 
-    @PreAuthorize("hasRole('MARKETING_ORGANIZER') or hasRole('HR_USER') or hasRole('ADMIN')")
+    @PreAuthorize("hasRole('MARKETING_ORGANIZER')")
     @PostMapping
     public ResponseEntity<EventResponse> createEvent(@Valid @RequestBody EventRequest eventRequest,
             Authentication authentication) {
