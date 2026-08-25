3dddcc86acada05101c963653bb21dcaa0fc526e
Chertes Victor <victor.chertes.vc@gmail.com>
2026-08-25 11:37:50 +0300

    fix:resolve artifact folder

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -9,7 +9,6 @@ import { httpInterceptor } from './core/interceptors/login.interceptor';
 import { HttpClient } from '@angular/common/http';
 import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { CustomTranslateLoader } from './core/translation/custom-translate.loader';
 import { initializeTranslation } from './core/translation/translation.initializer';
 import { initializeAuth } from './core/auth/auth.initializer';
@@ -18,7 +17,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideClientHydration(),
     provideHttpClient(withInterceptors([httpInterceptor])),
     provideAppInitializer(initializeAuth),
     provideAppInitializer(initializeTranslation),
