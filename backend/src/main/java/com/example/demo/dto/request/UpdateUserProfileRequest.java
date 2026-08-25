902462280246b033819c2a1d05fdc924b03ebda8
Gyurka Dora <dora.gyurka25@gmail.com>
2026-08-25 09:22:47 +0200

    fix:fixed request dto

--- backend/src/main/java/com/example/demo/dto/request/UpdateUserProfileRequest.java
+++ backend/src/main/java/com/example/demo/dto/request/UpdateUserProfileRequest.java
@@ -3,6 +3,8 @@ package com.example.demo.dto.request;
 import com.example.demo.model.Location;
 import com.example.demo.model.UserRole;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,14 +14,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UpdateUserProfileRequest {
 
+    @NotBlank(message = "First name is required")
     private String firstName;
 
+    @NotBlank(message = "Last name is required")
     private String lastName;
 
+    @NotBlank(message = "Email is required")
     private String email;
 
+    @NotNull(message = "Location is required")
     private Location location;
 
+    @NotNull(message = "User role is required")
     private UserRole role;
 
     private String imageBase64;
