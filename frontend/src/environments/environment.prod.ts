9232f8410111d8b2eef062821ed9d754783f51d1
Chertes Victor <victor.chertes.vc@gmail.com>
2026-08-25 15:19:26 +0300

    fix: resolve BE apiUrl

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'https://msyncg-backend.ashybeach-88b3ccc7.westeurope.azurecontainerapps.io',
+  apiUrl: 'https://msyncg-backend.ashybeach-88b3ccc7.westeurope.azurecontainerapps.io/api',
   production: true,
 };
